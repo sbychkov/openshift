6aaa03097fb3f4a952bb8c738c08b29b6233a20c
sbychkov <sbychkov@aport.ru>
2015-05-06 03:28:59 +0300

    Add shoppe engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,27 @@
 Rails.application.routes.draw do
+  #get 'orders/show'
 
+  #get 'orders/destroy'
+
+  mount Shoppe::Engine => '/shoppe'
+
+  get "product/:permalink", to: "products#show", as: "product"
+  post "product/:permalink", to: "products#buy", as: "buy"
+  get "basket", to: "orders#show"
+  delete "basket", to: "orders#destroy"
+
+  match "checkout", to: "orders#checkout", as: "checkout", via: [:get, :patch]
+  match "checkout/pay", to: "orders#payment", as: "checkout_payment", via: [:get, :post]
+  match "checkout/confirm", to: "orders#confirmation", as: "checkout_confirmation", via: [:get, :post]
+
+  get 'shop' => 'products#index'
   get 'main' => 'main#show'
 
-  get 'main/index'
+  root 'main#show'
 
-  get 'main/show'
+  #get 'main/index'
+
+  #get 'main/show'
 
 
 
